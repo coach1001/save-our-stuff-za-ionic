b40d7f8bf9d2b7f00211a9b59316aac046123ce7
Jorge Vergara <javebratt@gmail.com>
2018-09-16 12:50:08 -0500

    Adds feature to take pictures and upload to storage.

--- src/app/pages/event-detail/event-detail.page.ts
+++ src/app/pages/event-detail/event-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../../services/event/event.service';
 import { ActivatedRoute } from '@angular/router';
+import { Plugins, CameraResultType } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-event-detail',
@@ -9,7 +12,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EventDetailPage implements OnInit {
   public currentEvent: any = {};
-
+  public guestName = '';
+  public guestPicture: string = null;
   constructor(private eventService: EventService, private route: ActivatedRoute) {}
 
   ngOnInit() {
@@ -21,5 +25,32 @@ export class EventDetailPage implements OnInit {
         this.currentEvent = eventSnapshot.data();
         this.currentEvent.id = eventSnapshot.id;
       });
+  }
+
+  addGuest(guestName: string): void {
+    this.eventService
+      .addGuest(
+        guestName,
+        this.currentEvent.id,
+        this.currentEvent.price,
+        this.guestPicture
+      )
+      .then(() => {
+        this.guestName = '';
+        this.guestPicture = null;
+      });
+  }
+
+  async takePicture(): Promise<void> {
+    try {
+      const profilePicture = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Base64,
+      });
+      this.guestPicture = profilePicture.base64Data.slice(23);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
--- src/app/services/event/event.service.ts
+++ src/app/services/event/event.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 @Injectable({
   providedIn: 'root',
@@ -39,5 +40,46 @@ export class EventService {
 
   getEventDetail(eventId: string): firebase.firestore.DocumentReference {
     return this.eventListRef.doc(eventId);
+  }
+
+  addGuest(
+    guestName: string,
+    eventId: string,
+    eventPrice: number,
+    guestPicture: string = null
+  ): Promise<void> {
+    return this.eventListRef
+      .doc(eventId)
+      .collection('guestList')
+      .add({ guestName })
+      .then(newGuest => {
+        return firebase
+          .firestore()
+          .runTransaction(transaction => {
+            return transaction.get(this.eventListRef.doc(eventId)).then(eventDoc => {
+              const newRevenue = eventDoc.data().revenue + eventPrice;
+              transaction.update(this.eventListRef.doc(eventId), { revenue: newRevenue });
+            });
+          })
+          .then(() => {
+            if (guestPicture != null) {
+              const storageRef = firebase
+                .storage()
+                .ref(`/guestProfile/${newGuest.id}/profilePicture.png`);
+
+              return storageRef
+                .putString(guestPicture, 'base64', { contentType: 'image/png' })
+                .then(() => {
+                  return storageRef.getDownloadURL().then(downloadURL => {
+                    return this.eventListRef
+                      .doc(eventId)
+                      .collection('guestList')
+                      .doc(newGuest.id)
+                      .update({ profilePicture: downloadURL });
+                  });
+                });
+            }
+          });
+      });
   }
 }
