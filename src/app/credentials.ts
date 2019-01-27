3d9bbedb81d71966f69e254605e976be88569e9c
Jorge Vergara <javebratt@gmail.com>
2019-01-27 09:02:50 -0500

    Connects our app to Firebase

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDnlvRQ4UJl9ypzQjcQkMfIe8i9yEJ4oe0',
+  authDomain: 'firestore-event-manager.firebaseapp.com',
+  databaseURL: 'https://firestore-event-manager.firebaseio.com',
+  projectId: 'firestore-event-manager',
+  storageBucket: 'firestore-event-manager.appspot.com',
+  messagingSenderId: '823274361576',
+};
