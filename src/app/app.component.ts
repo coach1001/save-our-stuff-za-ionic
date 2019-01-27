cd34d47fb2f54f3c0b1f8144998967f38c97c122
Jorge Vergara <javebratt@gmail.com>
2019-01-27 08:39:44 -0500

    NUKE everything to move to V4 final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,26 @@
 import { Component } from '@angular/core';
-import { Plugins } from '@capacitor/core';
-import * as firebase from 'firebase/app';
-import { firebaseConfig } from './credentials';
 
-const { SplashScreen, StatusBar } = Plugins;
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor() {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    firebase.initializeApp(firebaseConfig);
-    SplashScreen.hide().catch(error => {
-      console.warn(error);
-    });
-
-    StatusBar.hide().catch(error => {
-      console.warn(error);
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
 }
